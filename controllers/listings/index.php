5c5dab784fef1060ed12568e348c580cb427e56d
patkis <p.kiss8888@gmail.com>
2024-10-03 16:49:19 +0200

    showing listings in the all listings page

--- controllers/listings/index.php
+++ controllers/listings/index.php
@@ -1,3 +1,8 @@
 <?php
 
-load_view('listings/index');
+$config = require base_path('config/db.php');
+$db = new Database($config);
+
+$listings = $db->query('SELECT * FROM listings')->fetchAll();
+
+load_view('listings/index', ['listings' => $listings]);
--- views/home.view.php
+++ views/home.view.php
@@ -8,7 +8,6 @@
   <div class="container mx-auto p-4 mt-4">
     <div class="text-center text-3xl mb-4 font-bold border border-gray-300 p-3">Recent Jobs</div>
     <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
-
       <?php foreach ($listings as $listing) : ?>
         <div class="rounded-lg shadow-md bg-white">
           <div class="p-4">
@@ -39,8 +38,6 @@
           </div>
         </div>
       <?php endforeach; ?>
-
-
     </div>
     <a href="/listings" class="block text-xl text-center">
       <i class="fa fa-arrow-alt-circle-right"></i>
